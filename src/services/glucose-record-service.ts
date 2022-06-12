c4407dd1d587d689bfaafa49f2c3b3c278611520
LUCAS ARAUJO EMMERICH <18969750703@cefet-rj.br>
2022-06-12 18:02:59 -0300

    adds comment

--- src/services/glucose-record-service.ts
+++ src/services/glucose-record-service.ts
@@ -126,8 +126,10 @@ async function getBestDosages(consumption: string, user_id: number) {
             from glucose_record
             where user_id = ${user_id}
             order by ranking
-        ) as q1 where q1.consumption = ${consumption}`;
+        ) as q1 where q1.consumption = ${consumption}
+        `;
 
+        //and TIMESTAMPDIFF(HOUR,q1.unformatted_prev_created_at, q1.unformatted_created_at) <= 5
 
         return result;
     }
